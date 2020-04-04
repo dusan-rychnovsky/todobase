a59c96ffca67e0ac2e02baa3087cf1d051d1d7b8
Dusan Rychnovsky <dusan.rychnovsky@gmail.com>
2020-04-04 17:51:06 +0200

    Create empty project.

---
+++ todobase/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace todobase.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
