7caeaab38f33d655043196f0920e7b2be2687ec1
Dusan Rychnovsky <dusan.rychnovsky@gmail.com>
2020-04-09 09:34:01 +0200

    Add xunit integration tests project.

---
+++ todobase.integrationtests/UnitTest1.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace todobase.integrationtests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public async Task Test1()
+        {
+            var builder = new WebHostBuilder()
+                .UseEnvironment("Development")
+                .UseStartup<Startup>();
+
+            var server = new TestServer(builder);
+            var client = server.CreateClient();
+
+            var response = await client.GetAsync("/");
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+            Assert.Contains("Hello World!", content);
+        }
+    }
+}
