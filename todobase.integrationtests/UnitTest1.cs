3e38101f9f6691b6df46e163ee66e5b79dacada2
Dusan Rychnovsky <dusan.rychnovsky@gmail.com>
2020-04-10 14:33:47 +0200

    Update integration test.

--- todobase.integrationtests/UnitTest1.cs
+++ todobase.integrationtests/UnitTest1.cs
@@ -1,6 +1,10 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Threading.Tasks;
+using todobase.Models;
 using Xunit;
 
 namespace todobase.integrationtests
@@ -10,6 +14,26 @@ namespace todobase.integrationtests
         [Fact]
         public async Task Test1()
         {
+            var dbConnection = new SqliteConnection("DataSource=:memory:");
+            dbConnection.Open();
+            Startup.DbConnection = dbConnection;
+
+            var dbOptions = new DbContextOptionsBuilder<AppDbContext>()
+                .UseSqlite(dbConnection)
+                .Options;
+
+            using (var dbContext = new AppDbContext(dbOptions))
+            {
+                await dbContext.Database.MigrateAsync();
+                await dbContext.Todos.AddRangeAsync(
+                    new[]
+                    {
+                        new Todo { TodoId = "1", Label = "Do the vacuuming." },
+                        new Todo { TodoId = "2", Label = "Feed the cat." }
+                    });
+                await dbContext.SaveChangesAsync();
+            }
+
             var builder = new WebHostBuilder()
                 .UseEnvironment("Development")
                 .UseStartup<Startup>();
@@ -18,10 +42,11 @@ namespace todobase.integrationtests
             var client = server.CreateClient();
 
             var response = await client.GetAsync("/");
-            response.EnsureSuccessStatusCode();
-
             var content = await response.Content.ReadAsStringAsync();
+            response.EnsureSuccessStatusCode();
             Assert.Contains("Todos:", content);
+            Assert.Contains("Do the vacuuming.", content);
+            Assert.Contains("Feed the cat.", content);
         }
     }
 }
